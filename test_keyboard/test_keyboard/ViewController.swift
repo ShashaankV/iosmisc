a0c04fccd12228679c0c9acf7086b221caaba7d6
svattiku <svattiku@dk12ar4003mbp11.local>
2018-12-01 18:16:11 -0500

    record keyboard input, barebones

---
+++ test_keyboard/test_keyboard/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  test_keyboard
+//
+//  Created by svattiku on 12/1/18.
+//  Copyright © 2018 svattiku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override var keyCommands: [UIKeyCommand]? {
+        return [
+            UIKeyCommand(input: UIKeyInputLeftArrow, modifierFlags: [], action: #selector(ViewController.LeftArrowPress)),
+//            UIKeyCommand(input: UIKeyInputUpArrow, modifierFlags: [], action: #selector(ViewController.LeftArrowPress)),
+            UIKeyCommand(input: UIKeyInputRightArrow, modifierFlags: [], action: #selector(ViewController.RightArrowPress))]
+    }
+    
+    @objc func LeftArrowPress(sender: UIKeyCommand) {
+//        if running {
+            print("Left")
+//            data.append( ("Left", Date().timeIntervalSinceReferenceDate) )
+//        }
+    }
+    
+    @objc func RightArrowPress(sender: UIKeyCommand) {
+        //        if running {
+        print("Right")
+        //            data.append( ("Left", Date().timeIntervalSinceReferenceDate) )
+        //        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
